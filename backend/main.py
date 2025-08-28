6ad3ad05bab43bfce7a1d2cdebbdf72528e624cd
Ilya Tokarev <buzinesstbox@gmail.com>
2025-08-28 21:49:49 +0500

    Обнулил весь код, запутался в веренице ошибок. Буду по кусочкам писать заново, по одному эндпоинту. Пока пустые файлы

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 
-
-load_dotenv()
 
 # роутеры
-from backend.api import vacancies, analytics, candidates, auth
+from backend.api import vacancies
 
 # БД
 from backend.database.connection import engine, Base
@@ -18,20 +14,9 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="NLP HR Assistant",
     description="Backend для MVP проекта по NLP-анализу вакансий и резюме",
-    version="0.1.0"
+    version="0.1.0",
 )
 
 
 # подключаем роутеры
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
-app.include_router(vacancies.router, prefix="/vacancies", tags=["vacancies"])
-app.include_router(analytics.router, prefix="/analytics", tags=["analytics"])
-
-# Event handlers (startup/shutdown)
-@app.on_event("startup")
-async def startup_event():
-    print("Запуск NLP HR Assistant Backend...")
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    print("Выключение сервера...")
+app.include_router(vacancies.router)
